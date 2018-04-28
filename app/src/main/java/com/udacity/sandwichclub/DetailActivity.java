f0fe20822c747f74ac04736d3d64a29231645735
David Rainey <david.rainey83@gmail.com>
2018-04-27 22:47:14 -0500

    Implement populating detail activity logic

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import com.udacity.sandwichclub.utils.StringUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView mOriginTextView;
+    private TextView mIngredientsTextView;
+    private TextView mAkaTextView;
+    private TextView mDescriptionTextView;
+
+    private Sandwich sandwichChosen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,9 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        // set the sandwich data returned to the member variable
+        this.sandwichChosen = sandwich;
+
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
@@ -57,6 +71,15 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        mAkaTextView = (TextView) findViewById(R.id.also_known_tv);
+        mDescriptionTextView = (TextView) findViewById(R.id.description_tv);
+        mOriginTextView = (TextView) findViewById(R.id.origin_tv);
+        mIngredientsTextView = (TextView) findViewById(R.id.ingredients_tv);
 
+        mAkaTextView.setText(StringUtils.getListAsCommaSeparatedString(sandwichChosen.getAlsoKnownAs()));
+        mIngredientsTextView.setText(StringUtils.getListAsCommaSeparatedString(sandwichChosen.getIngredients()));
+        mOriginTextView.setText(sandwichChosen.getPlaceOfOrigin());
+        mDescriptionTextView.setText(sandwichChosen.getDescription());
     }
+
 }
