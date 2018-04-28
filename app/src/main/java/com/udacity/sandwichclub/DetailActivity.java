15b3279282a845a7488f0a9c43296f69d5181fd9
David Rainey <david.rainey83@gmail.com>
2018-04-27 22:54:00 -0500

    Remove unnecessary import statement

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -12,8 +12,6 @@ import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 import com.udacity.sandwichclub.utils.StringUtils;
 
-import java.util.List;
-
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
